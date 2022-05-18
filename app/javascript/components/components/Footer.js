684ff6836404261dbbacee3db6cfc1e698cd0dc0
GeeeShep <gashep@yahoo.com>
2022-05-18 11:56:26 -0700

    home done

--- app/javascript/components/components/Footer.js
+++ app/javascript/components/components/Footer.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
   render() {
     return (
-      <h3>Footer</h3>
+      <h4>Brought to you by GeeShep </h4>
     )
   }
 }
